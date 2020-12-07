9cf23e2073bc0658dcb1c66c05f03bbe838a74e9
Sehbaz Rafik <sehbazrafik@MacBook-Pro.broadband>
2020-12-07 20:44:49 +0000

    creating course entity

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -1,8 +1,11 @@
 package com.springrest.springrest.controller;
 
+import com.springrest.springrest.entities.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MyController {
@@ -10,5 +13,9 @@ public class MyController {
     @GetMapping("/home")
     public String home(){
         return "Welcome to the Course Application.";
+    }
+
+    public List<Course> getCources(){
+        return null;
     }
 }
---
+++ src/main/java/com/springrest/springrest/entities/Course.java
@@ -0,0 +1,55 @@
+package com.springrest.springrest.entities;
+
+public class Course {
+    private Long courseID;
+    private String title;
+    private String description;
+
+    //making Constructor
+    public Course(Long courseID, String title, String description) {
+        this.courseID = courseID;
+        this.title = title;
+        this.description = description;
+    }
+
+    //generating the defauly constructor from super class
+    public Course(){
+        super();
+    }
+
+
+    //Getter and Setter method
+    public Long getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(Long courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //To String method
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseID=" + courseID +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
